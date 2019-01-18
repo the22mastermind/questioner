27100329843295005c0140a9fd4e9f6cdd45f829
themastermind <bertrandmasabo22@gmail.com>
2019-01-18 13:33:29 +0200

    Updated coveralls file

--- routes/question.routes.js
+++ routes/question.routes.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/controller');
 
 router.post('/:id/questions', controller.askQuestion);
 router.patch('/:meetupId/questions/:questionId/upvote', controller.upvoteQuestion);
-router.patch('/:id/downvote', controller.downvoteQuestion);
+router.patch('/:meetupId/questions/:questionId/downvote', controller.downvoteQuestion);
 router.post('/:meetupId/questions/:questionId/comment', controller.commentOnQuestion);
 
 module.exports = router;
